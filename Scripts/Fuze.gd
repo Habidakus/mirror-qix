9190063b1bcd8da070df20cd87a11790349427d4
Jason Abbott <woebane@gmail.com>
2024-11-11 19:40:13 -0800

    Have fuze respawn if trapped

--- Scripts/Fuze.gd
+++ Scripts/Fuze.gd
@@ -5,17 +5,37 @@ class_name Fuze
 var pos_on_field : Vector2i
 var play_state : PlayState = null
 var rotate : float = 0
+var max_respawn_time : float = 2
+var respawn_remaining : float
+var current_state : FuzeState
 
-func _process(delta : float) -> void:
-	rotate += delta
+enum FuzeState {MOVING, RESPAWNING}
 
 func init(ps : PlayState) -> void:
 	play_state = ps
-	pos_on_field = play_state.point_opposite_player()
+	change_state(FuzeState.RESPAWNING)
+
+func change_state(new_state : FuzeState) -> void:
+	if new_state == FuzeState.RESPAWNING:
+		current_state = FuzeState.RESPAWNING
+		pos_on_field = play_state.point_opposite_player()
+		respawn_remaining = max_respawn_time
+	else:
+		current_state = FuzeState.MOVING
 
 func move_fuze(delta : float) -> void:
+	rotate += (delta * 1.5)
+	if current_state == FuzeState.RESPAWNING:
+		respawn_remaining -= delta
+		if respawn_remaining < 0:
+			change_state(FuzeState.MOVING)
+		return
+		
 	var old_pos : Vector2i = pos_on_field
 	pos_on_field = get_new_pos(delta)
+	if current_state == FuzeState.RESPAWNING:
+		return
+
 	if (old_pos - pos_on_field).length() > 2:
 		print("Fuze teleported")
 	
@@ -39,17 +59,24 @@ func get_new_pos(_delta : float) -> Vector2i:
 				var start : Vector2i = play_state.outer_lines[i][0]
 				var end : Vector2i = play_state.outer_lines[i][1]
 				return advance_along(start, end, pos_on_field)
-	# TODO: Implement respawn
-	return play_state.point_opposite_player()
+
+	# The outer_line has been moved out from under us, teleport to opposite of player
+	change_state(FuzeState.RESPAWNING)
+	return pos_on_field
 
 func render(offset : Vector2) -> void:
 	var opp : Vector2 = offset + (pos_on_field as Vector2)
-	var rp : int = round(rotate) as int
-	var vfx_length : int = 4
-	if rp % 2 == 0:
-		play_state.draw_line(opp - Vector2(vfx_length, 0), opp + Vector2(vfx_length, 0), Color.GREEN)
-		play_state.draw_line(opp - Vector2(0, vfx_length), opp + Vector2(0, vfx_length), Color.GREEN)
-	elif rp % 2 == 1:
-		play_state.draw_line(opp - Vector2(vfx_length, vfx_length), opp + Vector2(vfx_length, vfx_length), Color.GREEN)
-		play_state.draw_line(opp - Vector2(-vfx_length, vfx_length), opp + Vector2(-vfx_length, vfx_length), Color.GREEN)
-	
+	var spine_count : int = 3
+	if current_state == FuzeState.RESPAWNING:
+		spine_count *= 3
+	for spine in range(0, spine_count):
+		var dir = Vector2(cos(rotate + spine), sin(rotate + spine))
+		if current_state == FuzeState.RESPAWNING:
+			var out_spin : float = 25.0 * respawn_remaining / max_respawn_time
+			var vfx_length : float = 6 + (4 * respawn_remaining / max_respawn_time)
+			var start : Vector2 = opp + dir * out_spin
+			var end : Vector2 = start + dir * vfx_length
+			play_state.draw_line(start, end, Color.GREEN)
+		else:
+			var vfx_length : float = 6
+			play_state.draw_line(opp + dir * vfx_length, opp - dir * vfx_length, Color.GREEN)
